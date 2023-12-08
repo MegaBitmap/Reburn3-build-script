f8dfeee864e9b4d90f3f1da391b44546ed32ae30
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2023-12-08 10:39:37 -0800

    revert to old commit

--- ScriptsForMSYS2/reburn3msys2.sh
+++ ScriptsForMSYS2/reburn3msys2.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
-echo Reburn3 Build Script for MinGW32 on Windows
-echo Installing and Updating Dependencies
+echo "Reburn3 Build Script for MinGW32 on Windows"
+echo "Installing and Updating Dependencies"
 pacman -Suy
 pacman -S cmake git mingw-w64-i686-gcc ninja
-echo Cloning Repository
-git clone https://github.com/reburndev/reburn3
-cd ./reburn3/cmake/
+echo "Cloning Repository"
+git clone https://github.com/reburndev/reburn3.git
+cd ./reburn3/
+git reset --hard 13cb56dae29ff61699c00698e634064f36426257
+git clean -df
+cd ./cmake/
 cp ./mingw-w64-i686.cmake ./mingw-w64-i686-windows.cmake
 sed -i 's/${TOOLCHAIN_PREFIX}-windres/windres/' ./mingw-w64-i686-windows.cmake
 mkdir ../build/
 cd ../build/
-echo Building with CMake for Ninja
+echo "Building with CMake for Ninja"
 cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-w64-i686-windows.cmake
-echo Building exe and dll
+echo "Building exe and dll"
 ninja
 mkdir ../exe/
 mv -t ../exe/ reburn3.exe reburn3inject{.dll,.dll.a}
 cd ../exe/
 cp /mingw32/bin/{libgcc_s_dw2-1.dll,libstdc++-6.dll,libwinpthread-1.dll} ./
-echo Done, Closing in 10 seconds
+echo "Done, Closing in 10 seconds"
 sleep 10
